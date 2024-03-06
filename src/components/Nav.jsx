e3ee04660b8f8393cc72ba6c7ab05af24105efde
John <johnzunigacastillo@gmail.com>
2024-03-06 15:31:54 +0800

    update nav link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Banner, Navbar } from 'flowbite-react';
 export default function Nav() {
     return (
       <Navbar fluid rounded className='bg-[#19191c] navigation sticky top-0 rounded-none py-4'>
-        <Navbar.Brand href="https://flowbite-react.com">
+        <Navbar.Brand href="">
         John Castillo
         </Navbar.Brand>
         <div className="flex md:order-2">
